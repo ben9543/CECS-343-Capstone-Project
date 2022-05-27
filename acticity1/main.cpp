96e67fe0e97e04fd487c714473b0b9b0bcbdeb06
Bundy-Mundi <bennie.ben095@gmail.com>
2022-05-27 12:10:24 -0700

    Initial Commit

---
+++ acticity1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  acticity1
+//
+//  Created by Ben Kweon on 2022/05/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
